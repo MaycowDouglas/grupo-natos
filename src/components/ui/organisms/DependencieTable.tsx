40998e875bde022c656ea384a8b7fc32c0d7d13a
Maycow Marques <melo.maycow@gmail.com>
2022-12-22 08:25:35 -0300

    fix: handle when user doesnt have dependencies and show the legal contact

--- src/components/ui/organisms/DependencieTable.tsx
+++ src/components/ui/organisms/DependencieTable.tsx
@@ -13,33 +13,35 @@ type Props = {
 
 export const DependencieTable = ({ sale, venture, company, building }: Props) => {
   const { user } = useUser()
+  const [total, setTotal] = useState<number>(0)
   const [dependencies, setDependencies] = useState([])
   const installments = useDependencies(user, sale, company, building)
 
   useEffect(() => {
     if (!installments.isLoading) {
+      let totalReaj = 0
+
       const filteredInstallments = installments.data.filter((installment: any, index: number) => {
         if (new Date(installment.Data_Prc) < new Date()) {
+          totalReaj += installment.ValorReaj
           return installment
         }
       })
 
+      setTotal(totalReaj)
       setDependencies(filteredInstallments)
     }
-  }, [installments.isLoading])
-
-  console.log(dependencies)
+  }, [installments.data, installments.isLoading])
 
   return (
-    <table className="w-full">
+    <table className="w-full mt-10">
       <thead>
         <tr className="text-left">
-          <th className="pb-6 text-gray-500 font-medium">Vencimento</th>
-          <th className="pb-6 hidden md:table-cell text-gray-500 font-medium">Empreendimento</th>
-          <th className="pb-6 text-gray-500 font-medium">Valor</th>
-          <th className="pb-6 ">
-            <span className="invisible">Ação</span>
-          </th>
+          <th className="pb-3 hidden md:table-cell text-gray-500 font-medium">Venda</th>
+          <th className="pb-3 md:table-cell text-gray-500 font-medium">Vencimento</th>
+          <th className="pb-3 hidden md:table-cell text-gray-500 font-medium">Unidade</th>
+          <th className="pb-3 hidden md:table-cell text-gray-500 font-medium">Valor da Parcela</th>
+          <th className="pb-3 text-gray-500 font-medium">Valor da Reajustado</th>
         </tr>
       </thead>
       <tbody>
@@ -52,79 +54,36 @@ export const DependencieTable = ({ sale, venture, company, building }: Props) =>
         ) : (
           dependencies.map((dependencie: any, index) => {
             return (
-              <tr key={index} className="">
-                <td className="py-2 hidden md:table-cell text-sm font-medium">
-                  {date.ISOStringToNormalDate(dependencie.Data_Prc)}
-                </td>
-                <td className="py-2 text-sm font-medium">{venture}</td>
-                <td className="py-2 hidden lg:table-cell text-sm font-medium">
-                  R$ {dependencie.Valor_Prc}
-                </td>
-              </tr>
+              <>
+                <tr key={index} className="">
+                  <td className="py-2 hidden md:table-cell text-sm font-medium">{sale}</td>
+                  <td className="py-2 md:table-cell text-sm font-medium">
+                    {date.ISOStringToNormalDate(dependencie.Data_Prc)}
+                  </td>
+                  <td className="py-2 hidden md:table-cell text-sm font-medium">{venture}</td>
+                  <td className="py-2 hidden md:table-cell text-sm font-medium">
+                    R$ {dependencie.Valor_Prc}
+                  </td>
+                  <td className="py-2 lg:table-cell text-sm font-medium">
+                    R$ {dependencie.ValorReaj}
+                  </td>
+                </tr>
+
+                {index === dependencies.length - 1 && (
+                  <tr key={index} className="border-t-2">
+                    <td
+                      colSpan={3}
+                      className="py-2 hidden md:table-cell text-sm font-medium uppercase"
+                    ></td>
+                    <td className="py-2 text-sm font-medium uppercase">Total</td>
+                    <td className="py-2 lg:table-cell text-sm font-medium">R$ {total}</td>
+                  </tr>
+                )}
+              </>
             )
           })
         )}
       </tbody>
-
-      {/* <tbody> */}
-      {/* {boletos.isLoading ? (
-          <tr>
-            <td className="text-center py-5" colSpan={7}>
-              Carregando...
-            </td>
-          </tr>
-        ) : (
-          boletos.data?.map((boleto, index) => {
-            const due_date = new Date(boleto.dataVencimento)
-            const issue_date = new Date(boleto.dataEmissao)
-
-            const print_date = [
-              {
-                d: issue_date.getDate() < 10 ? '0' + issue_date.getDate() : issue_date.getDate(),
-                m:
-                  issue_date.getMonth() + 1 < 10
-                    ? '0' + (issue_date.getMonth() + 1)
-                    : issue_date.getMonth() + 1,
-                y: issue_date.getFullYear(),
-              },
-              {
-                d: due_date.getDate() < 10 ? '0' + due_date.getDate() : due_date.getDate(),
-                m: due_date.getMonth() < 10 ? '0' + due_date.getMonth() : due_date.getMonth(),
-                y: due_date.getFullYear(),
-              },
-            ]
-
-            return (
-              boleto.obraParcela === activeVenture && (
-                <tr key={index} className="">
-                  <td className="py-2 hidden md:table-cell text-sm font-medium">{`${print_date[0].d}/${print_date[0].m}/${print_date[0].y}`}</td>
-                  <td className="py-2 text-sm font-medium">{`${print_date[1].d}/${print_date[1].m}/${print_date[1].y}`}</td>
-                  <td className="py-2 hidden lg:table-cell text-sm font-medium">
-                    {boleto.seuNumero}
-                  </td>
-                  <td className="py-2 hidden lg:table-cell text-sm font-medium">
-                    {boleto.codBanco}
-                  </td>
-                  <td className="py-2 hidden md:table-cell text-sm font-medium">
-                    {boleto.descricaoObra}
-                  </td>
-                  <td className="py-2 text-sm font-medium">
-                    <span>R$ {boleto.valorDocumento}</span>
-                  </td>
-                  <td className="py-2 ">
-                    <button
-                      onClick={() => handleViewBoleto(boleto.codBanco, boleto.seuNumero)}
-                      className="px-3 py-2 rounded-full bg-blue-700 text-xs text-white"
-                    >
-                      Visualizar
-                    </button>
-                  </td>
-                </tr>
-              )
-            )
-          })
-        )}
-      </tbody> */}
     </table>
   )
 }
--- src/pages/dashboard/dependencies.tsx
+++ src/pages/dashboard/dependencies.tsx
@@ -1,59 +1,103 @@
+import nProgress from 'nprogress'
 import { useEffect, useState } from 'react'
+import { AiOutlineLike } from 'react-icons/ai'
+import { Swiper, SwiperSlide } from 'swiper/react'
 
 import { TemplateDashboard } from '~/components/templates/Dashboard'
 import { Box } from '~/components/ui/atoms/Box'
+import { Button } from '~/components/ui/atoms/Button'
+import { ButtonLink } from '~/components/ui/atoms/ButtonLink'
+import { Text } from '~/components/ui/atoms/Text'
 import { DependencieTable } from '~/components/ui/organisms/DependencieTable'
 import useUser from '~/hooks/useUser'
 import useVentures from '~/hooks/useVentures'
+
+import 'swiper/css'
 
 const DependenciesPage = () => {
   const { user } = useUser()
   const ventures = useVentures(user)
 
-  const [activeVenture, setActiveVenture] = useState<string>('')
+  const [isLoading, setLoading] = useState(true)
+  const [selectedTab, setTab] = useState<string>('')
 
   useEffect(() => {
-    if (!ventures.isLoading) {
-      setActiveVenture(String(ventures.data?.[1].Obra_Ven))
-    }
-  }, [ventures])
+    nProgress.start()
 
-  console.log(ventures)
+    if (!ventures.isLoading) {
+      setLoading(false)
+    }
+
+    if (!isLoading) {
+      if (ventures.data && ventures.data.length > 1) {
+        setTab(ventures.data[1].Descr_obr)
+      }
+
+      nProgress.done()
+    }
+  }, [ventures.isLoading, ventures.data, isLoading])
 
   return (
     <TemplateDashboard title="Pendências" description="Todos as pendências">
-      <Box>
-        <div className="flex flex-wrap mb-8">
-          {ventures.isLoading
-            ? 'carregando...'
-            : ventures.data?.map(
-                (venture, index) =>
-                  index > 0 && (
-                    <button
-                      key={index}
-                      onClick={() => setActiveVenture(venture.Obra_Ven)}
-                      className="px-4 py-1 bg-blue-700 rounded-full text-white text-sm"
-                    >
-                      {venture.Descr_obr}
-                    </button>
-                  )
-              )}
-        </div>
+      {!isLoading && (
+        <>
+          {ventures.data && ventures.data.length > 1 ? (
+            <>
+              <Box className="lg:max-h-[400px] overflow-y-auto">
+                <Swiper spaceBetween={20} slidesPerView="auto">
+                  {ventures.data?.map((venture, index) => {
+                    return (
+                      index > 0 && (
+                        <SwiperSlide key={index} className="max-w-xs">
+                          <Button className="text-xs" isOutline={selectedTab !== venture.Descr_obr}>
+                            {venture.Empreendimento_ven}
+                          </Button>
+                        </SwiperSlide>
+                      )
+                    )
+                  })}
+                </Swiper>
 
-        {ventures.data?.map((venture, index) => {
-          return (
-            index > 0 && (
-              <DependencieTable
-                key={index}
-                venture={venture.Empreendimento_ven}
-                sale={venture.Num_Ven}
-                company={venture.Empresa_ven}
-                building={venture.Obra_Ven}
-              />
-            )
-          )
-        })}
-      </Box>
+                {ventures.data?.map((venture, index) => {
+                  return (
+                    index > 0 && (
+                      <DependencieTable
+                        key={index}
+                        venture={venture.Identificador_unid}
+                        sale={venture.Num_Ven}
+                        company={venture.Empresa_ven}
+                        building={venture.Obra_Ven}
+                      />
+                    )
+                  )
+                })}
+              </Box>
+              <Box className="bg-slate-300">
+                <div className="grid grid-cols-12 itens-center p-5">
+                  <Text className="col-span-6 text-gray-600">
+                    Prezado cliente, lembre-se que após 3 parcelas não pagas você terá que entrar em
+                    contato com nossa equipe de suporte para renegociar seu pagamento.
+                  </Text>
+                  <div className="col-span-6 text-right">
+                    <ButtonLink href="tel:(62)3121-7123" className="">
+                      Fale Conosco
+                    </ButtonLink>
+                  </div>
+                </div>
+              </Box>
+            </>
+          ) : (
+            <div className="w-full h-96 flex justify-center items-end">
+              <div className="flex flex-col items-center opacity-40 text-center">
+                <AiOutlineLike className="text-slate-600 text-9xl" />
+                <h2 className="text-3xl font-medium text-slate-600">
+                  Você não possui pagamentos pendentes!
+                </h2>
+              </div>
+            </div>
+          )}
+        </>
+      )}
     </TemplateDashboard>
   )
 }
