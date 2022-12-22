8981ddd5f1678a4d4111725a33509c7ddc952bc0
Maycow Marques <melo.maycow@gmail.com>
2022-12-22 08:24:04 -0300

    feat: notify the user if he has dependencies

--- src/pages/dashboard/posts/index.tsx
+++ src/pages/dashboard/posts/index.tsx
@@ -1,10 +1,17 @@
 import { NextPage } from 'next'
+import Link from 'next/link'
+import { useEffect, useState } from 'react'
+import { AiOutlineWhatsApp } from 'react-icons/ai'
+import { BsArrowRight, BsFillTelephoneFill } from 'react-icons/bs'
+import { FaTimes } from 'react-icons/fa'
 import { Swiper, SwiperSlide } from 'swiper/react'
 
 import { TemplateDashboard } from '~/components/templates/Dashboard'
 import { Box } from '~/components/ui/atoms/Box'
 import { Heading } from '~/components/ui/atoms/Heading'
+import { Text } from '~/components/ui/atoms/Text'
 import { NewsCard } from '~/components/ui/molecules/NewsCard'
+import useBoletos from '~/hooks/useBoletos'
 import useFeaturedPosts from '~/hooks/useFeaturedPosts'
 import usePosts from '~/hooks/usePosts'
 import useUser from '~/hooks/useUser'
@@ -15,9 +22,41 @@ const PostsPage: NextPage = () => {
   const { user } = useUser()
   const allPosts = usePosts(user)
   const featuredPosts = useFeaturedPosts(user)
+  const boletos = useBoletos(user)
+  const [modalIsVisible, setModalVisibility] = useState<boolean>(false)
+
+  useEffect(() => {
+    if (boletos.data && boletos.data.length > 3) {
+      setModalVisibility(true)
+    }
+  }, [boletos.data])
 
   return (
     <TemplateDashboard title="Notícias" description="Todos as notícias">
+      {modalIsVisible && (
+        <Box className="relative bg-slate-300">
+          <div className="flex flex-col md:flex-row md:px-5 gap-5 justify-between items-center text-center md:text-left">
+            <FaTimes
+              className="absolute top-3 right-3 cursor-pointer"
+              onClick={() => setModalVisibility(false)}
+            />
+            <div>
+              <Heading size={24}>Pagamento Pendente!</Heading>
+              <Text isMuted>Clique aqui para verificar sua pendências</Text>
+            </div>
+            <div>
+              <div className="flex flex-col gap-2">
+                <Link href="/dashboard/dependencies" legacyBehavior>
+                  <a className="inline-flex justify-center items-center gap-2 text-blue-700 font-medium">
+                    Ver pendências <BsArrowRight className="text-lg" strokeWidth={1} />
+                  </a>
+                </Link>
+              </div>
+            </div>
+          </div>
+        </Box>
+      )}
+
       <Box>
         <Heading className="mb-5">Destaques</Heading>
         <Swiper spaceBetween={20} slidesPerView="auto">
